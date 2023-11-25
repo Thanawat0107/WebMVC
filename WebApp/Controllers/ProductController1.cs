3530211803bd8d9eda5f26dd019b717a634d3ae9
Dell05 <Dell05@DESKTOP-F3L02L9>
2023-11-25 11:40:04 +0700

    Add project files.

---
+++ WebApp/Controllers/ProductController1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class ProductController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            var product = new List<Product>()
+            {
+                new Product() {id=1, name="Coffee1",price=100,amout=20},
+                new Product() {id=1, name="Coffee2",price=100,amout=20},
+
+            };
+            return View(product);
+        }
+    }
+}
---
+++ WebApp/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApp.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+        public int amout { get; set; }
+    }
+}
