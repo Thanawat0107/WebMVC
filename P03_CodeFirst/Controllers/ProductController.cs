c259ee144e88776a3c7c916b6c0e1b28eca0bb4c
Thanawat0107 <qwwasszxx@gmail.com>
2023-12-11 16:13:02 +0700

    Creatr and Update Product

--- P03_CodeFirst/Controllers/ProductController.cs
+++ P03_CodeFirst/Controllers/ProductController.cs
@@ -32,9 +32,35 @@ namespace P03_CodeFirst.Controllers
             return RedirectToAction("Index");  
         }
 
+        [HttpPost] //กด submit ให้มาที่นี้
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost] //กด submit ให้มาที่นี้
+        public IActionResult Create(Product product)
+        {
+            if (!ModelState.IsValid) return View();
+            ps.Add(product);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var product = ps.GetById(id);
+            if (product == null)
+            {
+                TempData["Narney"] = true; //นำไปใช้ที่หน้า View
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (!ModelState.IsValid) return View();
+            ps.Update(product);
+            return RedirectToAction("Index");
         }
     }
 }
--- P03_CodeFirst/Services/ProductServices.cs
+++ P03_CodeFirst/Services/ProductServices.cs
@@ -18,6 +18,12 @@ namespace P03_CodeFirst.Services
             if (_db.Products.Count() == 0 ) GenerateProduct();
         }
 
+        public void Add(Product product)
+        {
+            _db.Products.Add(product);
+            _db.SaveChanges();
+        }
+
         public void Delete(Product product)
         {
             _db.Products.Remove(product); //memory
@@ -44,7 +50,7 @@ namespace P03_CodeFirst.Services
         public IEnumerable<Product> GetAll()
         {
             var test= _db.Products.ToList();
-            return _db.Products.ToList();
+            return _db.Products.OrderByDescending(p => p.Id).ToList();
         }
 
         public Product GetById(int id)
@@ -52,6 +58,12 @@ namespace P03_CodeFirst.Services
             var product = _db.Products.Find(id);
 
             return product;
+        }
+
+        public void Update(Product product)
+        {
+            _db.Products.Update(product);
+            _db.SaveChanges();
         }
     }
 }
