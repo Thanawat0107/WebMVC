46d7163c0d4135bddfd2bb1219c7663eedac56af
Thanawat0107 <qwwasszxx@gmail.com>
2023-12-23 10:05:13 +0700

    Register and Add new Property

---
+++ P07_AuthenDB/Controllers/ProductController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace P07_AuthenDB.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
