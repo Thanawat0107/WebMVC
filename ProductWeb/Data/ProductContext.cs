0043f6588117a13f49ab8956e1ab2c9cf2b505ac
Thanawat0107 <qwwasszxx@gmail.com>
2024-01-20 13:56:46 +0700

    Add Product Table

--- ProductWeb/Data/ProductContext.cs
+++ ProductWeb/Data/ProductContext.cs
@@ -19,5 +19,6 @@ namespace ProductWeb.Data
 
         public DbSet<User> Users {  get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
---
+++ ProductWeb/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductWeb.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [ValidateNever]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [Display(Name = "Category Name")]
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        [ValidateNever]
+        public Category Category { get; set; }
+    }
+}
